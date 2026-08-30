2dd664558c40d851ce3807e01de4e92e8cb9852d
Made Vidyatma Adhi Krisna <krisnahmbtn@gmail.com>
2026-08-30 18:13:33 +0700

    Initial Commit

---
+++ POC-OCR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POC-OCR
+//
+//  Created by Made Vidyatma Adhi Krisna on 30/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POC-OCR/POC_OCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_OCRApp.swift
+//  POC-OCR
+//
+//  Created by Made Vidyatma Adhi Krisna on 30/08/26.
+//
+
+import SwiftUI
+
+@main
+struct POC_OCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
